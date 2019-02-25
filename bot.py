a4e459a8a645ad59729e1bfab6d4f77072e5db97
KaptainMidnight <alexartemkin173@mail.ru>
2019-02-26 00:31:20 +0400

    Added new game casino

--- bot.py
+++ bot.py
@@ -1,24 +1,43 @@
 from telegram.ext import *
 import db
+import random
 
-
-updater = Updater(token="727202433:AAGXUfFgx9qDML4t2jibu5dRQcIMqlilXFk")
+updater = Updater(token="720140452:AAHJ2fQXSp9WZuz39OdV_eMrShwa7rLZcmc")
 dispatcher = updater.dispatcher
 
 
 def startCommand(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Привет, напиши мне 'баланс' что бы узнать баланс")
+    bot.send_message(chat_id=update.message.chat_id, text="Напиши 'помощь' что бы посмотреть все команды")
 
 
 def textMessage(bot, update):
     response = update.message.text.lower()
-
+    user_status = db.GetUserStats(update.message.chat_id)
     if response == "баланс":
-        b = db.GetUserStats(update.message.chat_id)
         try:
-            bot.send_message(chat_id=update.message.chat_id, text=f"Твой баланс: {b['money']}")
-        except TypeError:
-            print("Error")
+            bot.send_message(chat_id=update.message.chat_id, text=f"Твой баланс: {user_status['money']}")
+        except:
+            bot.send_message(chat_id=update.message.chat_id, text="Ты зарегистрирован, напиши еще раз баланс")
+    elif response == "профиль":
+        bot.send_message(chat_id=update.message.chat_id,
+                         text=f"🖥Твой ID: {user_status['id']}\n💰Баланс: {user_status['money']}")
+    elif response == "помощь":
+        bot.send_message(chat_id=update.message.chat_id,
+                         text="Профиль - для просмотра профиля\nБаланс - просмотр баланса\nКазино сумма - для игры в казино")
+    elif response.split()[0] == "казино":
+        if int(user_status['money']) < int(response.split()[1]):
+            bot.send_message(chat_id=update.message.chat_id, text="У тебя не хватает денег!")
+            return
+        else:
+            r = random.randint(0, 100)
+            if r >= 50:
+                db.addMoney(update.message.chat_id, int(response.split()[1]), 1)
+                bot.send_message(chat_id=update.message.chat_id, text="Поздравляю, ты выйграл")
+            else:
+                db.addMoney(update.message.chat_id, int(response.split()[1]), 2)
+                bot.send_message(chat_id=update.message.chat_id, text="Прости, но ты проиграл")
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text="Я тебя не понял")
 
 
 start_command_handler = CommandHandler("start", startCommand)
--- db.py
+++ db.py
@@ -19,3 +19,19 @@ def GetUserStats(user_id):
                     return row
     finally:
         connect.close()
+
+
+def addMoney(user_id, money, status):
+    connect = connection.getConnect()
+    try:
+        with connect.cursor() as cursor:
+            result = cursor.execute(f"SELECT * FROM `accounts` WHERE `uid` = {user_id}")
+            row = cursor.fetchone()
+            if status == 1 and result == 1:
+                row['money'] += money
+            elif status == 2 and result >= 1:
+                row['money'] -= money
+            cursor.execute(f"UPDATE accounts SET money= {row['money']} WHERE uid = {user_id}")
+            connect.commit()
+    except:
+        connect.close()
